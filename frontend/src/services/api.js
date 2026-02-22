135a38d2bddeca6d3ebe6ea3bf02fdab92d72bd1
Vaibhav Patil <vaibhav7315patil@gmail.com>
2026-02-22 23:13:00 +0530

    update API base URL and add Vercel configuration file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://member-management-project.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
